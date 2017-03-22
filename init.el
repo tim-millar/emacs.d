93a3eac8b0af97500abe94be8d00621fbab57570
Tim Millar <timothy.millar@onthebeach.co.uk>
2017-03-22 16:56:50 +0000

    Added lots of rails and projectile setup.

--- init.el
+++ init.el
@@ -28,6 +28,9 @@
 (load-theme 'creamsody t)
 (creamsody-modeline)
 
+;; load source code pro font
+(set-frame-font "Source Code Pro 11")
+
 ;; icons
 (use-package all-the-icons
   :ensure t
@@ -58,9 +61,13 @@
 (setq sentence-end-double-space nil)
 (setq default-fill-column 80)
 
-;; backups
-(setq backup-directory-alist `(("." . "~/.emacs.d/backups")) )
-(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t)) )
+;; Backups
+(setq backup-directory-alist
+      '(("." . "~/.emacs.d/backups")))
+(setq auto-save-file-name-transforms
+      `((".*" "~/.emacs.d/auto-save-list/" t)))
+;; Stop backup changing file creation data
+(setq backup-by-copying t)
 
 ;; winner mode
 (winner-mode 1)
@@ -69,9 +76,15 @@
 (setq windmove-wrap-around t)
 
 ;; no tabs
-(setq-default indent-tabs-mode nil)
+(setq default-indent-tabs-mode nil)
 
 (require 'tramp)
+
+(use-package undo-tree
+  :ensure t
+  :diminish undo-tree-mode
+  :config
+  (global-undo-tree-mode))
 
 ;; ==============================
 ;; dired
@@ -81,7 +94,7 @@
 (setq dired-recursive-copies (quote always)) ; “always” means no asking
 (setq dired-recursive-deletes (quote top)) ; “top” means ask once
 (setq dired-dwim-target t)
-; dired-details
+; dired-detailsauto-save
 (use-package dired-details
   :ensure t
   :config
@@ -116,7 +129,7 @@
               ("C-j" . ivy-next-line)
               ("C-k" . ivy-previous-line)
               )
-
+  :config
   (setq ivy-use-virtual-buffers t)   ; extend searching to bookmarks and …
   (setq ivy-height 20)               ; set height of the ivy window
   (setq ivy-count-format "(%d/%d) ") ; count format, from the ivy help page
@@ -125,6 +138,21 @@
 (use-package counsel
   :ensure t)
 
+(use-package projectile
+  :ensure t
+  :config
+  (use-package counsel-projectile
+    :ensure t
+    :config
+    (counsel-projectile-on))
+  (use-package projectile-rails
+    :ensure t
+    :diminish projectile-rails-mode
+    :config
+    (projectile-rails-global-mode))
+
+  (projectile-mode))
+
 (use-package evil
   :ensure t
   :config
@@ -132,12 +160,6 @@
 
 (use-package magit
   :ensure t)
-
-(use-package undo-tree
-  :ensure t
-  :diminish undo-tree-mode
-  :init
-  (global-undo-tree-mode))
 
 (use-package general
   :ensure t
@@ -150,36 +172,90 @@
 
    "SPC" '(counsel-M-x :which-key "M-x")
 
-   "h" '(:ignore t :which-key "help")
+   "h"  '(:ignore t :which-key "help")
    "ha" 'apropos-command
    "hd" 'apropos-documentation
    "hi" 'info
    "hk" 'describe-key
-   "hf" 'describe-function
-   "hv" 'describe-variable
+   "hf" 'counsel-describe-function
+   "hv" 'counsel-describe-variable
    "hb" 'describe-bindings
    "hm" 'describe-mode
    "hP" 'describe-package
    "hr" 'info-emacs-manual
    "hs" 'describe-syntax
+   "hl" 'counsel-find-library
 
-   "b" '(:ignore t :which-key "buffers")
+   "b"  '(:ignore t :which-key "buffers")
    "bb" 'ivy-switch-buffer
    "ss" 'save-buffer
    "bo" 'ivy-switch-buffer-other-window
+   "bi" 'ibuffer
 
-   "f" '(:ignore t :which-key "files")
+   "f"  '(:ignore t :which-key "files")
    "ff" 'counsel-find-file
    "fo" 'find-file-other-window
-   "fl" 'counsel-find-library
 
-   "d" '(:ignore t :which-key "directories")
+   "d"  '(:ignore t :which-key "directories")
    "dd" 'ido-dired
+   "do" 'dired-other-window
 
-   "g" '(:ignore t :which-key "git")
+   "g"  '(:ignore t :which-key "git")
    "gs" 'magit-status
    "gg" 'counsel-git-grep
+   "gf" 'counsel-git
+
+   "p"   '(:ignore t :which-key "projectile")
+   "pb"  '(counsel-projectile-switch-to-buffer :which-key "switch-buffer")
+   "pd"  '(counsel-projectile-find-dir :which-key "find-dir")
+   "pD"  '(projectile-dired :which-key "dired")
+   "pI"  '(projectile-ibuffer :which-key "iBuffer")
+   "pf"  '(counsel-projectile-find-file :which-key "find-file")
+   "pp"  '(counsel-projectile-switch-project :which-key "switch-project")
+   "pss" '(counsel-projectile-ag :which-key "ag")
+
+   "r"  '(:ignore t :which-key "rails")
+   "r!" '(:ignore t :which-key "run")
+
+   "rg" '(:ignore t :which-key "goto")
+   "rgd" '(projectile-rails-goto-schema :which-key "goto-schema")
+   "rgf" '(projectile-rails-goto-file-at-point :which-key "goto-file-at-point")
+   "rgg" '(projectile-rails-goto-gemfile :which-key "goto-gemfile")
+   "rgh" '(projectile-rails-goto-spec-helper :which-key "goto-spec-helper")
+   "rgr" '(projectile-rails-goto-routes :which-key "goto-routes")
+   "rgs" '(projectile-rails-goto-seeds :which-key "goto-seed")
+
+   "rc" '(projectile-rails-find-controller :which-key "find-controller")
+   "rC" '(projectile-rails-find-current-controller :which-key "find-current-controller")
+   "rm" '(projectile-rails-find-model :which-key "find-model")
+   "rM" '(projectile-rails-find-current-model :which-key "find-current-model")
+   "rp" '(projectile-rails-find-spec :which-key "find-spec")
+   "rP" '(projectile-rails-find-current-spec :which-key "find-current-spec")
+   "rr" '(projectile-rails-console :which-key "console")
+   "rv" '(projectile-rails-find-view :which-key "find-view")
+   "rV" '(projectile-rails-find-current-view :which-key "find-current-view")
    ))
+
+;; ==============================
+;; Language Support
+;; ==============================
+
+(use-package web-mode
+  :ensure t)
+
+(use-package rvm
+  :ensure t
+  :config
+  (rvm-use-default))
+
+(use-package yaml-mode
+  :ensure t)
+
+(use-package rpsec-mode
+  :ensure t)
+
+(use-package bundler
+  :ensure t)
 
 ;; ==============================
 ;; auto-generated config
